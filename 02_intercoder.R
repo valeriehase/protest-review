a10b46fba0d24ff2c919d1ab89d526b3624ff7ac
Valerie <valerie.hase@web.de>
2023-08-02 15:18:04 +0200

    added info for sample intercoder_1

--- 02_intercoder.R
+++ 02_intercoder.R
@@ -16,13 +16,21 @@ sample_prelim <- read_xlsx(here("data", paste0("savedrecs_SSCI_dup_flagged_DT202
                               "WOS:000209845600001")) %>% #remove duplicates (prefer issued articles over early access, i.e., go by date)
   mutate(ID = paste0("ID", 1:nrow(.)))
 
+# Add link from "Gegencheck" file ---------------------------------------------------------------
+sample_prelim <- sample_prelim %>%
+  select(-DOI.Link) %>% #delete because empty in original file
+  right_join(read.csv2(here("data", paste0("Gegencheck.csv"))) %>%
+               filter(UT..Unique.WOS.ID. %in% sample_prelim$UT..Unique.WOS.ID.) %>%
+               select(UT..Unique.WOS.ID., DOI.Link))
+
 # Intercodertest 1: 100 articles, stratified sample ---------------------------------------------------------------
 
 intercoder_1 <- sample_prelim %>%
   group_by(Publication.Year) %>%
   slice_sample(n = 7) %>%
-  select(c(Author.Full.Names, Article.Title, Source.Title, Abstract,
-           Publication.Year, Volume, Issue)) %>%
+  select(c(ID, Author.Full.Names, Article.Title, Source.Title, Abstract,
+           Publication.Year, Volume, Issue, 
+           UT..Unique.WOS.ID., DOI, DOI.Link)) %>%
   sample_n(., 105)
 
 #write.csv2(intercoder_1, "data/intercoder_1.csv", row.names = FALSE)
