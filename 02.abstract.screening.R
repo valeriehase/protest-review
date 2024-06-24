f133bfca3ab994856b6470d80e5b1c0e841339ac
valeriehase <valerie.hase@web.de>
2024-06-24 17:31:20 +0200

    update re-sampling for equal sample sizes

--- 02.abstract.screening.R
+++ 02.abstract.screening.R
@@ -2,9 +2,11 @@
 #
 # Sort into CSS/non-CSS sample & abstract screening
 # Author: Valerie Hase
-# Date: 2024-05-01
+# Date: 2024-06-24
 #
 ########################
+
+`%!in%` <- Negate(`%in%`)
 
 #### Step 2.1: Sort sample into CSS/non-CSS samples #### 
 
@@ -72,8 +74,7 @@ ggarrange(css.sample %>%
             ggplot(aes(x = year, y = n)) + geom_line() +
             ggtitle(paste0("Non CSS Sample (N = ", nrow(non.css.sample), ")")) + theme_bw())
 
-
-####  Step 2.2 Intercodertest 1 Sample: N = 20 articles, stratified by CSS vs. non-CSS #### 
+####  Step 2.2 Intercodertest 1 Sample: N = 20 articles, stratified by CSS vs. non-CSS (OLD ID)#### 
 
 #sample.abstracts.1 <- css.sample %>%
 #  
@@ -106,7 +107,7 @@ ggarrange(css.sample %>%
 #write.csv2(sample.abstracts.1, "codings/abstract_screening_ah_mm_vh/intercoder.1.mm.csv", row.names = FALSE)
 #write.csv2(sample.abstracts.1, "codings/abstract_screening_ah_mm_vh/intercoder.1.vh.csv", row.names = FALSE)
 
-####  Step 2.3  Intercodertest 2 Sample: N = 20 articles, stratified by CSS vs. non-CSS #### 
+####  Step 2.3  Intercodertest 2 Sample: N = 20 articles, stratified by CSS vs. non-CSS (OLD ID) #### 
 
 #sample.abstracts.2 <- css.sample %>%
 #  
@@ -148,7 +149,7 @@ ggarrange(css.sample %>%
 #write.csv2(sample.abstracts.2, "codings/abstract_screening_ah_mm_vh/intercoder.2.mm.csv", row.names = FALSE)
 #write.csv2(sample.abstracts.2, "codings/abstract_screening_ah_mm_vh/intercoder.2.vh.csv", row.names = FALSE)
 
-####  Step 2.4 Intercodertest 3 Sample: N = 30 articles, stratified by CSS vs. non-CSS #### 
+####  Step 2.4 Intercodertest 3 Sample: N = 30 articles, stratified by CSS vs. non-CSS (OLD ID) #### 
 
 #sample.abstracts.3 <- css.sample %>%
 #  
@@ -198,7 +199,7 @@ ggarrange(css.sample %>%
 #write.csv2(sample.abstracts.3, "codings/abstract_screening_ah_mm_vh/intercoder.3.mm.csv", row.names = FALSE)
 #write.csv2(sample.abstracts.3, "codings/abstract_screening_ah_mm_vh/intercoder.3.vh.csv", row.names = FALSE)
 
-####  Step 2.5  Intercodertest 4 Sample: N = 30 articles, stratified by CSS vs. non-CSS #### 
+####  Step 2.5  Intercodertest 4 Sample: N = 30 articles, stratified by CSS vs. non-CSS (OLD ID) #### 
 
 #sample.abstracts.4 <- css.sample %>%
 #  
@@ -323,7 +324,7 @@ ggarrange(css.sample %>%
 #write.csv2(sample.abstracts.5, "codings/abstract_screening_ah_mm_vh/intercoder.5.vh.csv", row.names = FALSE)
 
 ####  Step 2.7 Test reliability with latest coding (4 and 5) #### 
-intercoder <- read_xlsx("codings/abstract_screening_ah_mm_vh/vergleich5.xlsx") %>%
+intercoder_cases <- read_xlsx("codings/abstract_screening_ah_mm_vh/vergleich5.xlsx") %>%
   
   #reduce to necessary variables
   select(id_unique, starts_with("protest"), 
@@ -357,7 +358,7 @@ intercoder <- read_xlsx("codings/abstract_screening_ah_mm_vh/vergleich5.xlsx") %
                                 protest_vh == 1 & type_vh == 1,
                                 1)) %>%
   
-  #bind codig 4
+  #bind coding 4
   rbind(read_xlsx("codings/abstract_screening_ah_mm_vh/vergleich4.xlsx") %>%
           
           #reduce to necessary variables
@@ -402,8 +403,10 @@ intercoder <- read_xlsx("codings/abstract_screening_ah_mm_vh/vergleich5.xlsx") %
   #to wide again
   pivot_wider(names_from = variable, values_from = c(value))
 
-intercoder %>% 
-  test_icr(id_unique, coder, protest, method, type, empirical, inclusion)
+#save relevant intercoder values
+intercoder <- intercoder_cases %>% 
+  test_icr(id_unique, coder, protest, method, type, empirical, inclusion) %>%
+  filter(Variable != "empirical" & Variable != "inclusion")
 
 ####  Step 2.7 Validate CSS keywords #### 
 
@@ -434,102 +437,193 @@ validation <- full_join(automated_coding, manual_coding) %>%
          coding_manual = as.factor(coding_manual))
 
 #get result of validation
-confusionMatrix(data = validation$coding_automated, 
-                reference = validation$coding_manual, 
-                mode = "prec_recall", 
-                positive = "1")
+validation <- confusionMatrix(data = validation$coding_automated, 
+                              reference = validation$coding_manual, 
+                              mode = "prec_recall", 
+                              positive = "1")
 
 #clean house
 rm(automated_coding, manual_coding)
 
-####  Step 2.8 Draw samples for coding ### 
-random <- non.css.sample %>%
-  slice_sample(n = nrow(css.sample)) 
+####  Step 2.8 Draw samples for coding #### 
+#random <- non.css.sample %>%
+#  slice_sample(n = nrow(css.sample)) 
 
 #check out distribution of samples over time
-ggarrange(css.sample %>%
-            count(year) %>%
-            ggplot(aes(x = year, y = n)) + geom_line() +
-            ggtitle(paste0("CSS Sample (N = ", nrow(css.sample), ")")) + theme_bw(),
-          random %>%
-            count(year) %>%
-            ggplot(aes(x = year, y = n)) + geom_line() +
-            ggtitle(paste0("Non CSS Sample (N = ", nrow(non.css.sample), ")")) + theme_bw())
+#ggarrange(css.sample %>%
+#            count(year) %>%
+#            ggplot(aes(x = year, y = n)) + geom_line() +
+#            ggtitle(paste0("CSS Sample (N = ", nrow(css.sample), ")")) + theme_bw(),
+#          random %>%
+#            count(year) %>%
+#            ggplot(aes(x = year, y = n)) + geom_line() +
+#            ggtitle(paste0("Non CSS Sample (N = ", nrow(non.css.sample), ")")) + theme_bw())
 
 #check spread over years: randomly enough
-css.sample %>%
-  mutate(sample = "CSS") %>%
-  full_join(random %>%
-              mutate(sample = "non-CSS")) %>%
-  group_by(sample) %>%
-  count(year) %>%
-  ungroup %>%
+#css.sample %>%
+#  mutate(sample = "CSS") %>%
+#  full_join(random %>%
+#              mutate(sample = "non-CSS")) %>%
+#  group_by(sample) %>%
+#  count(year) %>%
+#  ungroup %>%
 
-  #to wide again
-  pivot_wider(names_from = c(sample), values_from = c(n))
+#  #to wide again
+#  pivot_wider(names_from = c(sample), values_from = c(n))
 
 #create sample for coding
-coding_abstracts <- css.sample %>%
-  mutate(sample = "CSS") %>%
-  full_join(random %>%
-              mutate(sample = "non-CSS")) %>%
-  
-  #create link, replace with NA if doi missing
-  mutate(link = paste0("https://doi.org/", doi),
-         link = replace(link,
-                        is.na(doi),
-                        NA)) %>%
-  #sort randomly
-  slice_sample(n = nrow(random) + nrow(css.sample)) %>%
-  
-  #assign coder
-  mutate(coder = NA,
-         coder = replace(coder,
-                         id_unique %in% intercoder$id_unique,
-                         "already coded (reliability)"),
-         coder = replace(coder,
-                         is.na(coder),
-                         rep(c("AH", "MM", "VH"), 226)))
+#coding_abstracts <- css.sample %>%
+#  mutate(sample = "CSS") %>%
+#  full_join(random %>%
+#              mutate(sample = "non-CSS")) %>%
+#  
+#  #create link, replace with NA if doi missing
+#  mutate(link = paste0("https://doi.org/", doi),
+#         link = replace(link,
+#                        is.na(doi),
+#                        NA)) %>%
+#  #sort randomly
+#  slice_sample(n = nrow(random) + nrow(css.sample)) %>%
+#  
+#  #assign coder
+#  mutate(coder = NA,
+#         coder = replace(coder,
+#                         id_unique %in% intercoder$id_unique,
+#                         "already coded (reliability)"),
+#         coder = replace(coder,
+#                         is.na(coder),
+#                         rep(c("AH", "MM", "VH"), 226)))
 
 #create codings sheets
-coding_abstracts_ah <- coding_abstracts %>%
-  filter(coder == "AH") %>%
-  
-  #select necessary variables
-  select(id_unique, authors, title, abstract, keywords, link) %>%
-  
-  #add empty coding variables
-  mutate(coder = NA,
-         protest = NA,
-         method = NA,
-         type = NA)
+# coding_abstracts_ah <- coding_abstracts %>%
+#  filter(coder == "AH") %>%
+#  
+#  #select necessary variables
+#  select(id_unique, authors, title, abstract, keywords, link) %>%
+#  
+#  #add empty coding variables
+#  mutate(coder = NA,
+#         protest = NA,
+#         method = NA,
+#         type = NA)
 
-coding_abstracts_mm <- coding_abstracts %>%
-  filter(coder == "MM") %>%
-  
-  #select necessary variables
-  select(id_unique, authors, title, abstract, keywords, link) %>%
-  
-  #add empty coding variables
-  mutate(coder = NA,
-         protest = NA,
-         method = NA,
-         type = NA)
+#coding_abstracts_mm <- coding_abstracts %>%
+#  filter(coder == "MM") %>%
+#  
+#  #select necessary variables
+#  select(id_unique, authors, title, abstract, keywords, link) %>%
+#  
+#  #add empty coding variables
+#  mutate(coder = NA,
+#         protest = NA,
+#         method = NA,
+#         type = NA)
 
-coding_abstracts_vh <- coding_abstracts %>%
-  filter(coder == "VH") %>%
-  
-  #select necessary variables
-  select(id_unique, authors, title, abstract, keywords, link) %>%
-  
-  #add empty coding variables
-  mutate(coder = NA,
-         protest = NA,
-         method = NA,
-         type = NA)
+#coding_abstracts_vh <- coding_abstracts %>%
+#  filter(coder == "VH") %>%
+#  
+#  #select necessary variables
+#  select(id_unique, authors, title, abstract, keywords, link) %>%
+#  
+#  #add empty coding variables
+#  mutate(coder = NA,
+#         protest = NA,
+#         method = NA,
+#         type = NA)
 
 #write out for manual coding
-write.csv2(coding_abstracts_ah, "codings/abstract_screening_ah_mm_vh/coding.ah.csv", row.names = FALSE)
-write.csv2(coding_abstracts_mm, "codings/abstract_screening_ah_mm_vh/coding.mm.csv", row.names = FALSE)
-write.csv2(coding_abstracts_vh, "codings/abstract_screening_ah_mm_vh/coding.vh.csv", row.names = FALSE)
-    
+#write.csv2(coding_abstracts_ah, "codings/abstract_screening_ah_mm_vh/coding.ah.csv", row.names = FALSE)
+#write.csv2(coding_abstracts_mm, "codings/abstract_screening_ah_mm_vh/coding.mm.csv", row.names = FALSE)
+#write.csv2(coding_abstracts_vh, "codings/abstract_screening_ah_mm_vh/coding.vh.csv", row.names = FALSE)
+
+####  Step 2.9 Read in & clean initial abstract coding #### 
+
+#read in files by three coders
+coding_abstracts <- read_xlsx("codings/abstract_screening_ah_mm_vh/coding.ah.xlsx") %>%
+  rbind(read_xlsx("codings/abstract_screening_ah_mm_vh/coding.mm.xlsx")) %>%
+  rbind(read_xlsx("codings/abstract_screening_ah_mm_vh/coding.vh.xlsx"))
+
+#clean
+coding_abstracts <- coding_abstracts %>%
+  
+  #mark inaccessible studies (paper not available)
+  mutate(protest = replace(protest,
+                           id_unique %in% c("ID2626", "ID1295", "ID2376"),
+                           NA),
+         method = replace(method,
+                          id_unique %in% c("ID2626", "ID1295", "ID2376"),
+                          NA),
+         type = replace(type,
+                        id_unique %in% c("ID2626", "ID1295", "ID2376"),
+                        NA)) %>%
+  
+  #correct coding error for method variable
+  mutate(method = replace(method,
+                          method == 2,
+                          1)) %>%
+  
+  #correct coding error for type variable
+  mutate(type = replace(type,
+                        type == 0,
+                        2)) %>%
+  
+  #create overarching inclusion criterion
+  mutate(inclusion = 0,
+         inclusion = replace(inclusion,
+                             protest == 1 & type == 1,
+                             1))
+
+#identify inaccessible studies
+n_inaccessible <- coding_abstracts %>%
+  
+  filter(is.na(protest) & is.na(method) & is.na(type)) %>%
+  nrow()
+
+#identify relevant sample
+coding_abstracts_relevant <- coding_abstracts %>%
+  
+  #identify relevance
+  filter(inclusion == 1) %>%
+  
+  #add other meta data
+  left_join(wos.abstracts %>%
+              select(id_unique, source.title, source.type, source.conference,
+                     author.addresses, author.affiliations, year))
+
+####  Step 2.10 Add files for second coding round to get similar sample size for CSS and non-CSS sample (N = 207 each) #### 
+
+#based on step 2.9 (initial coding of CSS sample, we have to add around N = 49 non-CSS pieces 
+# as well as a missing CSS file to get equal sample sizes for both CSS and non-CSS sample
+
+#coding_abstracts_added <- css.sample %>%
+# 
+#  #add missing CSS piece, likely coded as CSS, meaning that CSS sample: N = 207
+#  filter(id_unique %!in% coding_abstracts$id_unique & id_unique %!in% intercoder_cases$id_unique) %>%
+#  mutate(sample = "CSS") %>%
+#  
+#  #add around 250 non-CSS pieces (but only code until N = 207 "reached" so CSS size = non-CSS size)
+#  #we oversample once so we do not have to do it again
+#  full_join(non.css.sample %>%
+#              filter(id_unique %!in% coding_abstracts) %>%
+#              slice_sample(n = 250) %>%
+#              mutate(sample = "non-CSS")) %>%
+#  
+#  # create link, replace with NA if doi missing
+#  mutate(link = paste0("https://doi.org/", doi),
+#         link = replace(link,
+#                        is.na(doi),
+#                        NA)) %>%
+#  
+#  #  select necessary variables
+#  select(id_unique, authors, title, abstract, keywords, link) %>%
+#  
+#  #  add empty coding variables
+#  mutate(coder = "MM",
+#         protest = NA,
+#         method = NA,
+#         type = NA)
+
+#write out for manual coding
+#write.csv2(coding_abstracts_added, "codings/abstract_screening_ah_mm_vh/coding.mm.added.csv", row.names = FALSE)
+
+####  Step 2.11 Create final sample #### 
--- main.R
+++ main.R
@@ -2,7 +2,7 @@
 #
 # Main Script for Lit Review Protest
 # Author: Valerie Hase
-# Date: 2024-05-01
+# Date: 2024-06-24
 #
 ########################
 
@@ -31,5 +31,72 @@ library("caret")
 #draws sample for intercoder tests & coding of abstracts
 
 load("working_spaces/01.load.data.RDATA")
-#source("02.abstract.screening.R")
+source("02.abstract.screening.R")
+
+##### 2.1 report details for method/appendix section #####
+
+#initial amount of identified studies
+n_deduplicated
+
+#validation of search string
+validation
+
+#initial sample of CSS vs. non-CSS studies
+nrow(css.sample)
+nrow(non.css.sample)
+
+#coded sample of CSS vs. non-CSS studies for abstracts
+nrow(coding_abstracts) #tbd
+
+#reliability
+intercoder
+
+#number of relevant studies
+nrow(coding_abstracts_relevant) #tbd
+
+#distribution of CSS vs. non-CSS over time
+ggarrange(coding_abstracts_relevant %>%
+            filter(method == 1) %>%
+            count(year) %>%
+            ggplot(aes(x = year, y = n)) + geom_line() +
+            ggtitle(paste0("CSS Sample (N = ", nrow(css.sample), ")")) + theme_bw(),
+          coding_abstracts_relevant %>%
+            filter(method == 0) %>%
+            count(year) %>%
+            ggplot(aes(x = year, y = n)) + geom_line() +
+            ggtitle(paste0("Non CSS Sample (N = ", nrow(non.css.sample), ")")) + theme_bw())
+
+coding_abstracts_relevant %>%
+  group_by(method) %>%
+  count(year) %>%
+  ungroup %>%
+  mutate(method = replace(method,
+                          method == 0,
+                          "non-CSS"),
+         method = replace(method,
+                          method == 1,
+                          "CSS")) %>%
+  pivot_wider(names_from = c(method), values_from = c(n)) %>%
+  arrange(as.numeric(year))
+
+##### 2.1 report details for method/appendix section #####
+
+flow.chart <- flow_exclusions(
+  incl_counts = c(nrow(wos.abstracts), 
+                  nrow(coding_abstracts), 
+                  nrow(coding_abstracts %>%
+                         filter(!is.na(protest) & !is.na(method) & !is.na(type))),
+                  nrow(coding_abstracts_relevant)),
+  total_label = "Deduplicated articles from WoS",
+  incl_labels = c("By-method stratified sample", 
+                  "Accessible full papers",
+                  "Relevant full papers"),
+  excl_labels = c("Removal due by-method\nstratified sampling", 
+                  "Removal due to\ninaccessible full paper", 
+                  "Removal due to\nirrelevance (manual coding)"),
+)
+
+#has to be saved manually (500, 300 as size for export)
+flow.chart
+
 #save.image("working_spaces/02.abstract.screening.RDATA")
