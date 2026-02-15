c5fe9aee30d152e3e5820ffcfbe4f600f5dea6ae
milzner <miriam.milzner@gmail.com>
2026-02-15 15:57:04 +0100

    improved readability

--- main.R
+++ main.R
@@ -49,54 +49,52 @@ RUN <- list(
 )
 
 # 01 WoS Import ----------------------------------------------------------------
+
 # Loads Web of Science data (2019-2023, downloaded on 03.04.2024)
 # Does some initial data cleaning (removing duplicates and other irrelevant abstracts)
-
 if (RUN$wos_import) source(here("scripts/01.load.wos.data.R"))
 
 # 02 Abstract Screening --------------------------------------------------------
-#samples sample 1 (CSS) and sample 2 (non-CSS) based on search terms
-#draws sample for intercoder tests & coding of abstracts
 
+# Samples sample 1 (CSS) and sample 2 (non-CSS) based on search terms
+# Draws sample for intercoder tests & coding of abstracts
 if (RUN$abstract_screening) source(here("scripts/02.abstract.screening.R"))
 
-# 03a Reliability Test Masks (generate sheets) ---------------------------------
+# 03 Reliability Tests ---------------------------------------------------------
 
+# Generate sheets
 if (RUN$reliability_masks) source(here("scripts/03a.reliability.masks.R"))
 
-# 03b Reliability Tests (compute metrics) --------------------------------------
-
+# Compute metrics
 if (RUN$reliability_tests) source(here("scripts/03b.reliability.testing.R"))
 
 # 04 Final Coding Masks --------------------------------------------------------
-# Creates final coding masks for coders
 
+# Creates final coding masks for coders
 if (RUN$final_masks) source(here("scripts/04.final.coding.masks.R"))
 
 # 05 Deduplication -------------------------------------------------------------
-# Exports dupes table + applies remove/keep-list
 
+# Exports dupes table + applies remove/keep-list
 if (RUN$deduplication) source(here("scripts/05.deduplication.R"))
 
-# 06a Final Data Cleaning (general) --------------------------------------------
-# Uses deduplicated coded sample, applies general data cleaning + logs
+# 06 Data Cleaning ------------------------------------------------------------
 
+# Uses deduplicated coded sample, applies general data cleaning + logs
 if (RUN$final_clean) source(here("scripts/06a.data.cleaning.general.R"))
 
-# 06b Final Data Cleaning (comments) --------------------------------------------
 # Uses cleaned coded sample, checks all coders comments + logs
-
 if (RUN$final_clean) source(here("scripts/06b.data.cleaning.comments.R"))
 
-# 06c Final Data Cleaning (codes) --------------------------------------------
 # Uses cleaned coded sample with checked comments, checks rare codes and code distributions + logs
-
 if (RUN$final_clean) source(here("scripts/06c.data.cleaning.codes.R"))
 
 # 07 Analysis Output -----------------------------------------------------------
 
-if (RUN$figures) source(here("scripts/07.analysis.figures.R"))
-if (RUN$tables)  source(here("scripts/07.analysis.tables.R"))
+# Generates Figures
+if (RUN$figures) source(here("scripts/07a.analysis.figures.R"))
+# Generates Tables
+if (RUN$tables)  source(here("scripts/07b.analysis.tables.R"))
 
 message("main completed: ", format(Sys.time(), "%Y-%m-%d %H:%M:%S"))
 message("---- SESSION INFO ----")
