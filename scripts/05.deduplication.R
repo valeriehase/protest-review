6e598d10761e94bf8ca93b055af6d7f136680267
vhase <valerie.hase@web.de>
2026-03-30 11:26:26 +0200

    Initial changes: 1) consistent naming (df to full_paper_coding), 2) load data were necessary from scripts 01, 02, etc.

--- scripts/05.deduplication.R
+++ scripts/05.deduplication.R
@@ -1,27 +1,124 @@
 #
 # Deduplication 
-# Date: 2025-10-20
+# Date: 2026-03-30
 #
 # Setup ------------------------------------------------------------------------
-
-source(here::here("R/paths.R"))
-source(here::here("R/config.R"))
-source(here::here("R/helpers.R"))
 
 library(readxl)
 library(dplyr)
 library(openxlsx)
 
-# Load Input -------------------------------------------------------------------
+source(here::here("R/paths.R"))
+source(here::here("R/config.R"))
+source(here::here("R/helpers.R"))
+
+# Input data -------------------------------------------------------------------
+
+# Load data from 01, if necessary
+
+if (!exists("wos_abstracts", inherits = TRUE)) {
+  
+  # Directory
+  dir_path <- PATHS$int
+  
+  # List matching files
+  files <- list.files(
+    dir_path,
+    pattern = "^01_wos_abstracts_clean(_\\d{8}_\\d{4})?\\.rds$",
+    full.names = TRUE
+  )
+  
+  if (length(files) == 0) {
+    stop("No matching WoS abstracts files found, please run script 01.load.wos.data first.")
+  }
+  
+  # Extract timestamps (if present)
+  timestamps <- sub(
+    ".*_(\\d{8}_\\d{4})\\.rds$",
+    "\\1",
+    files
+  )
+  
+  # Convert to POSIXct
+  timestamps <- as.POSIXct(
+    timestamps,
+    format = "%Y%m%d_%H%M",
+    tz = "UTC"
+  )
+  
+  # Select latest file
+  latest_file <- files[which.max(timestamps)]
+  
+  message("Loading cleaned WoS abstracts from: ", timestamps[which.max(timestamps)])
+  
+  wos_abstracts <- readRDS(latest_file)
+  
+  #clean house
+  rm(timestamps, dir_path, files, latest_file)
+}
+
+# Load data from 02, if necessary
+
+objs <- c("intercoder_abstracts", "validation_abstracts", "coding_abstracts", "coding_abstracts_relevant")
+
+# run if at least one of these files is missing
+if (!all(sapply(objs, exists, inherits = TRUE))) {
+  
+  # Directory
+  dir_path <- PATHS$int
+  
+  # List matching files
+  files <- list.files(
+    dir_path,
+    pattern = "^02_abstract_screening_clean(_\\d{8}_\\d{4})?\\.rds$",
+    full.names = TRUE
+  )
+  
+  if (length(files) == 0) {
+    stop("No matching cleaned abstract coding found, please run script 02.abstract.screening first.")
+  }
+  
+  # Extract timestamps (if present)
+  timestamps <- sub(
+    ".*_(\\d{8}_\\d{4})\\.rds$",
+    "\\1",
+    files
+  )
+  
+  # Convert to POSIXct
+  timestamps <- as.POSIXct(
+    timestamps,
+    format = "%Y%m%d_%H%M",
+    tz = "UTC"
+  )
+  
+  # Select latest file
+  latest_file <- files[which.max(timestamps)]
+  
+  message("Loading abstract codings from: ", timestamps[which.max(timestamps)])
+  
+  data <- readRDS(latest_file)
+  
+  # separate objects
+  intercoder_abstracts <- data$intercoder_abstracts
+  validation_abstracts  <- data$validation_abstracts
+  coding_abstracts <- data$coding_abstracts
+  coding_abstracts_relevant <- data$coding_abstracts_relevant
+  
+  #clean house
+  rm(objs, timestamps, dir_path, files, latest_file)
+}
+
+# Load input data from full paper coding
 
 input_file <- require_file(IN$coded_full_sample, "coded full-paper sample")
 
 message("Reading coded full sample from: ", input_file)
-df <- readxl::read_excel(input_file)
+coding_full_paper <- readxl::read_excel(input_file)
 
 # 5.1 Identify Remaining Duplicates --------------------------------------------
 
-df <- df %>%
+coding_full_paper <- coding_full_paper %>%
   dplyr::add_count(id_unique, name = "n") %>%
   dplyr::group_by(id_unique) %>%
   dplyr::mutate(
@@ -35,7 +132,7 @@ df <- df %>%
   dplyr::ungroup()
 
 
-dupes <- df %>%
+dupes <- coding_full_paper %>%
   dplyr::group_by(id_unique) %>%
   dplyr::filter(dplyr::n() > 1) %>%
   dplyr::ungroup() %>%
@@ -81,10 +178,10 @@ to_remove <- dupes_checked %>%
   dplyr::filter(decision == 0) %>%
   dplyr::pull(id_unique_dup)
 
-df_deduplicated <- df %>%
+coding_full_paper_deduplicated <- coding_full_paper %>%
   dplyr::filter(!id_unique_dup %in% to_remove)
 
-removed_duplicates <- df %>%
+removed_duplicates <- coding_full_paper %>%
   dplyr::filter(id_unique_dup %in% to_remove)
 
 # Output -----------------------------------------------------------------------
@@ -92,14 +189,14 @@ removed_duplicates <- df %>%
 out_dir <- PATHS$int
 
 out_removed_dupes   <- file.path(out_dir, "05_deduplication_removed_dupes.xlsx")
-out_df_deduplicated <- file.path(out_dir, "05_deduplication_full_paper_sample_deduplicated.xlsx")
+out_coding_full_paper_deduplicated <- file.path(out_dir, "05_deduplication_full_paper_sample_deduplicated.xlsx")
 
-openxlsx::write.xlsx(df_deduplicated, out_df_deduplicated, overwrite = T)
+openxlsx::write.xlsx(coding_full_paper_deduplicated, out_coding_full_paper_deduplicated, overwrite = T)
 openxlsx::write.xlsx(removed_duplicates, out_removed_dupes, overwrite = T)
 
 message("05 completed.")
 message("- Duplicated IDs detected: ", n_dupe_ids)
-message("- Deduplicated dataset at: ", out_df_deduplicated)
+message("- Deduplicated dataset at: ", out_coding_full_paper_deduplicated)
 message("- Removed duplicates at: ", out_removed_dupes)
 
 
