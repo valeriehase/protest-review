0e8c1507f29ea748e81c677345a530f2c4ee6ee6
vhase <valerie.hase@web.de>
2026-05-03 13:29:35 +0200

    Make script executable for peer review

--- main.R
+++ main.R
@@ -1,6 +1,10 @@
 #
 # Main Script Systematic Literature Review "Online Protest"
-# Date: 2026-03-30
+# Date: 2026-05-03
+
+# We provide the full code but, due to copyright reasons, can only provide data to run everything from
+# Step 7 (analysis) forward
+
 #
 # Setup ------------------------------------------------------------------------
 
@@ -52,42 +56,42 @@ RUN <- list(
 
 # Loads Web of Science data (2019-2023, downloaded on 03.04.2024)
 # Does some initial data cleaning (removing duplicates and other irrelevant abstracts)
-if (RUN$wos_import) source(here::here("scripts/01.load.wos.data.R"))
+#if (RUN$wos_import) source(here::here("scripts/01.load.wos.data.R"))
 
 # 02 Abstract Screening --------------------------------------------------------
 
 # Samples sample 1 (CSS) and sample 2 (non-CSS) based on search terms
 # Draws sample for intercoder tests & coding of abstracts
-if (RUN$abstract_screening) source(here::here("scripts/02.abstract.screening.R"))
+#if (RUN$abstract_screening) source(here::here("scripts/02.abstract.screening.R"))
 
 # 03 Reliability Tests ---------------------------------------------------------
 
 # Generate sheets
-if (RUN$reliability_masks) source(here("scripts/03a.reliability.masks.R"))
+#if (RUN$reliability_masks) source(here("scripts/03a.reliability.masks.R"))
 
 # Compute metrics
-if (RUN$reliability_tests) source(here("scripts/03b.reliability.testing.R"))
+#if (RUN$reliability_tests) source(here("scripts/03b.reliability.testing.R"))
 
 # 04 Final Coding Masks --------------------------------------------------------
 
 # Creates final coding masks for coders
-if (RUN$final_masks) source(here("scripts/04.final.coding.masks.R"))
+#if (RUN$final_masks) source(here("scripts/04.final.coding.masks.R"))
 
 # 05 Deduplication -------------------------------------------------------------
 
 # Exports dupes table + applies remove/keep-list
-if (RUN$deduplication) source(here("scripts/05.deduplication.R"))
+#if (RUN$deduplication) source(here("scripts/05.deduplication.R"))
 
 # 06 Data Cleaning ------------------------------------------------------------
 
 # Uses deduplicated coded sample, applies general data cleaning + logs
-if (RUN$final_clean) source(here("scripts/06a.data.cleaning.general.R"))
+#if (RUN$final_clean) source(here("scripts/06a.data.cleaning.general.R"))
 
 # Uses cleaned coded sample, checks all coders comments + logs
-if (RUN$final_clean) source(here("scripts/06b.data.cleaning.comments.R"))
+#if (RUN$final_clean) source(here("scripts/06b.data.cleaning.comments.R"))
 
 # Uses cleaned coded sample with checked comments, checks rare codes and code distributions + logs
-if (RUN$final_clean) source(here("scripts/06c.data.cleaning.codes.R"))
+#if (RUN$final_clean) source(here("scripts/06c.data.cleaning.codes.R"))
 
 # 07 Analysis Output -----------------------------------------------------------
 
